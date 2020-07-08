cd2d356cfb75b680e5322390c2dba7c618b7c05d
SpadeSe <2921576608@qq.com>
2020-07-08 15:59:45 +0800

    修改了一下WorkFlowBase

--- Assets/Scripts/WorkFlowBase.cs
+++ Assets/Scripts/WorkFlowBase.cs
@@ -7,7 +7,7 @@ public class WorkFlowBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Good Bye");
     }
 
     // Update is called once per frame
