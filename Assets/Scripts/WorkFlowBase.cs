8bd6a3e1bd28d172de5582575485cde266a81a72
SpadeSe <2921576608@qq.com>
2020-07-08 16:37:49 +0800

    把车控室的模型放到了场景里面

--- Assets/Scripts/WorkFlowBase.cs
+++ Assets/Scripts/WorkFlowBase.cs
@@ -8,7 +8,7 @@ public class WorkFlowBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello");
+        Debug.Log("Good Bye");
     }
 
     // Update is called once per frame
