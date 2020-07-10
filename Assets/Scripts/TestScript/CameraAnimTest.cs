ed67c4ecd98561e3fecd2659d52ce737cd1d61b0
SOLnene <SOLnene@users.noreply.github.com>
2020-07-11 01:04:22 +0800

    结合fpsaio的模型动画

---
+++ Assets/Scripts/TestScript/CameraAnimTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimTest : MonoBehaviour
+{
+    public Transform P_head;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (P_head != null)
+        {
+            transform.position = P_head.position;
+        }
+    }
+}
